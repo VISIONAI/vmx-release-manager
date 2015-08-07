0cf90f4fd9df581ebeaa587db80b47c72642c44f
Tomasz Malisiewicz <tom@vision.ai>
2015-08-07 06:03:33 -0400

    use semver sorting as opposed to dates

--- scripts/make_latest.sh
+++ scripts/make_latest.sh
@@ -9,11 +9,12 @@
 # $ROOTDIR/$NAME/$PLATFORM/, which in the case of VMX is something
 # like: /www/vmx/VMXserver/Linux/
 #
-# 1. This script will generate symliks for the "latest" version as well
-# as the "stable" version, where "latest" has the most recent
+# 1. This script will generate symliks for the "latest" version as
+# well as the "stable" version, where "latest" has the most recent
 # timestamp and "stable" has no dashses in the filename.  This works
 # when git tagging with stable version numbers like v0.1.2 and using
-# `git describe --tags --dirty` for the build version.
+# `git describe --tags --dirty` for the build version. Sorting is done
+# via `sort -v`
 #
 # 2. This script will generate a size+MD5hash+filename listing for the
 # latest build by appending .files.txt to the latest build name. This
@@ -29,7 +30,7 @@
 # downloaded version matches these checksums and hasn't been tampered
 # with.
 # 
-# Copyright 2014 Tomasz Malisiewicz (tom@vision.ai)
+# Copyright 2015 Tomasz Malisiewicz (tom@vision.ai)
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 build_name platform"
@@ -50,9 +51,11 @@ else
 fi
 
 echo 'Name='$NAME 'Platform='$PLATFORM
-TARBALLS=`find $ROOTDIR/$NAME/$PLATFORM/ -type f -name "*.tar.gz" -o -type f -name "*.pkg"`
-latest=`ls -ltr $TARBALLS | awk {'print $9'} | tail -1`
+TARBALLS=`find $ROOTDIR/$NAME/$PLATFORM/ -type f -name "*.tar.gz" -o -type f -name "*.pkg" | sort -V`
+latest=`find $ROOTDIR/$NAME/$PLATFORM/ -type f -name "*.tar.gz" -o -type f -name "*.pkg" | sort -V | tail -1`
+#latest=`ls -ltr $TARBALLS | awk {'print $9'} | tail -1`
 echo 'Latest is' $latest
+
 EXT=tar.gz
 if [ "`basename $latest .pkg`.pkg" == "`basename $latest`" ]; then
     EXT=pkg
@@ -72,15 +75,22 @@ fi
 rm $ROOTDIR/$NAME/$PLATFORM/${NAME}${PSTRING}.latest.${EXT} 2>/dev/null
 ln -s `basename $latest` $ROOTDIR/$NAME/$PLATFORM/${NAME}${PSTRING}.latest.${EXT}
 
-stable=`find $ROOTDIR/$NAME/$PLATFORM/ -type f -name "*.tar.gz" -o -type f -name "*.pkg" | xargs ls -ltr | awk {'print $9'} | grep -v "-" | tail -1`
+#stable=`find $ROOTDIR/$NAME/$PLATFORM/ -type f -name "*.tar.gz" -o -type f -name "*.pkg" | xargs ls -ltr | awk {'print $9'} | grep -v "-" | tail -1`
+stable=`find $ROOTDIR/$NAME/$PLATFORM/ -type f -name "*.tar.gz" -o -type f -name "*.pkg" | sort -V | grep -v "-" | tail -1`
 echo 'Stable is ' $stable
 rm $ROOTDIR/$NAME/$PLATFORM/${NAME}${PSTRING}.stable.${EXT} 2>/dev/null
 ln -s `basename $stable` $ROOTDIR/$NAME/$PLATFORM/${NAME}${PSTRING}.stable.${EXT}
 
+CWD=`pwd`
 cd $ROOTDIR/$NAME/$PLATFORM/
-md5sum *.${EXT} > MD5SUMS.txt 2>/dev/null
-sha1sum *.${EXT} > SHA1SUMS.txt 2>/dev/null
-sha256sum *.${EXT} > SHA256SUMS.txt 2>/dev/null
+FL=`ls *.${EXT} | grep "latest"`
+FS=`ls *.${EXT} | grep "stable"`
+F=`ls *.${EXT} | sort -V -r | grep -v "latest" | grep -v "stable"`
+md5sum ${FL} ${FS} ${F} > MD5SUMS.txt 2>/dev/null
+sha1sum ${FL} ${FS} ${F} > SHA1SUMS.txt 2>/dev/null
+sha256sum ${FL} ${FS} ${F} > SHA256SUMS.txt 2>/dev/null
+echo "Making json"
+${CWD}/md52json.sh MD5SUMS.txt > MD5SUMS.json
 cd - >/dev/null
 
 
